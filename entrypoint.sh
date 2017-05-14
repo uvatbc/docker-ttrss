5c945d9e8471ab14cce736961262e3c163180af9
Uv <yuvraaj@gmail.com>
2017-05-14 16:05:43 -0700

    Sleep 10 seconds so that the PG container init can complete

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# If you stop on errors, the psql calls fail on second and subsequent "docker-compose up"
 #set -e
 set -x
 
@@ -11,6 +12,9 @@ set -x
 : "${TTRSS_FEED_CRYPT_KEY:=""}"
 : "${TTRSS_HOST_URL:="http://localhost/"}"
 
+# Slep for 10 seconds so that the PG container can finish its own initialization
+sleep 10
+
 RESULT=$(PGPASSWORD=$TTRSS_DB_PASS psql -h "$TTRSS_DB_HOST" -U "$TTRSS_DB_USER" -q -c '\dt' "$TTRSS_DB_NAME" | \
     grep "rows" | grep -v "\(0 rows\)") > /dev/null 2>&1
 
@@ -21,6 +25,7 @@ if [[ "$RESULT" == "" ]]; then
     PGPASSWORD=$TTRSS_DB_PASS psql -h "$TTRSS_DB_HOST" -U "$TTRSS_DB_USER" -q "$TTRSS_DB_NAME" < /var/www/html/ttrss/schema/ttrss_schema_pgsql.sql
 fi
 
+# All these sed's are run as sudo because they're writing to /var/www/html/ttrss
 sudo sed -i -e "s/DB_HOST_VALUE/$TTRSS_DB_HOST/" /var/www/html/ttrss/config.php
 sudo sed -i -e "s/DB_USER_VALUE/$TTRSS_DB_USER/" /var/www/html/ttrss/config.php
 sudo sed -i -e "s/DB_NAME_VALUE/$TTRSS_DB_NAME/" /var/www/html/ttrss/config.php
@@ -29,4 +34,5 @@ sudo sed -i -e "s/DB_PORT_VALUE/$TTRSS_DB_PORT/" /var/www/html/ttrss/config.php
 sudo sed -i -e "s/FEED_CRYPT_KEY_VALUE/$TTRSS_FEED_CRYPT_KEY/" /var/www/html/ttrss/config.php
 sudo sed -i -e "s#HOST_URL#$TTRSS_HOST_URL#" /var/www/html/ttrss/config.php
 
+# Run the original command as sudo: This containers default user is non-root
 exec sudo "$@"
